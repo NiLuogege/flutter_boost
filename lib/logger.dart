cb57a488f081db4e1f8de013ead1df41c127905e
luoChen <hank.luo@xianghuanji.com>
2021-06-18 14:10:27 +0800

    添加 logger 方便调试

--- lib/logger.dart
+++ lib/logger.dart
@@ -1,12 +1,19 @@
+import 'package:logger/logger.dart' as Log;
+
+// ignore: avoid_classes_with_only_static_members
 class Logger {
+  static var logger = Log.Logger(printer: Log.PrettyPrinter(methodCount: 0));
+
   static void log(String msg) {
     assert(() {
-      print('FlutterBoost#$msg');
+      // print('FlutterBoost#$msg');
+      logger.e('FlutterBoost#$msg');
       return true;
     }());
   }
 
   static void error(String msg) {
-    print('FlutterBoost#$msg');
+    // print('FlutterBoost#$msg');
+    logger.e('FlutterBoost#$msg');
   }
 }
